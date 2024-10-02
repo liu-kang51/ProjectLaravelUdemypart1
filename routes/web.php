1e6552f9c98da5d21e2ef231b46a315fd9e117a3
liukang <pokaiqwe@gmail.com>
2024-10-02 15:13:34 +0700

    update baru

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "is about home";
+});     
+
+Route::get('/user/{id}/{slug}', function ($id, $slug) {
+    return "hello user " . $id. " " . $slug;
+});
