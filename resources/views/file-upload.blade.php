c2577befb6a1695c5b5fb3368d651ff1e0e099f5
liukang <pokaiqwe@gmail.com>
2024-10-18 17:05:49 +0700

    test

--- resources/views/file-upload.blade.php
+++ resources/views/file-upload.blade.php
@@ -7,9 +7,8 @@
 <form class="mx-auto mt-3" action="{{ route('file.store') }}" method="POST" enctype="multipart/form-data">
   @csrf   {{-- important this is token in laravel and selfly --}}
     <div class="mb-3">
-      <label for="" class="form-label block mb-3 text-base font-bold text-gray-900 dark:text-dark">FILE UPLOAD</label>
+      <label for="" class="form-label block mb-3 text-base font-bold text-gray-900 dark:text-dark">FILE UPLOAD mantap</label>
       <input type="file" id="" class="form-control  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2" name="file"/>
-
     </div>
     
     <div>
